5433d1f76070152b8c09e783e43e8b013b515a19
Sujal <sujalnageshrishi05122003@gmail.com>
2024-05-08 19:51:53 +0530

    now you can check any sentence also

--- palindrome/strPalin.py
+++ palindrome/strPalin.py
@@ -1,11 +1,24 @@
-def ispali(i,n,string):
+def isPalindrome(s):
 
-    if i>=n/2:
-        return True
-    if string[i]!=string[n-i-1]:
-        return False
-    return ispali(i+1,n,string)
+    def alnum(string1):
+        alnums = ""
+        for i in string1:
+            if i.upper():
+                i=i.lower()
+            if i.isalnum():
+                alnums+=i
+        return(alnums)
 
-string = input("Enter the string : ")
-ans = ispali(0,len(string),string)
-print(ans)
+    def ispali(i,n,string):
+        if i>=n/2:
+            return True
+        if string[i]!=string[n-i-1]:
+            return False
+        return ispali(i+1,n,string)
+
+    st = alnum(s)
+    return ispali(0,len(st),st)
+    
+string = input("Enter the sentence or a word for checking palindrome : ")
+print(isPalindrome(string))
+
