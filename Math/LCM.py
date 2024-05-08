205c5b4ea68d8645ed8f9c7c5eb6e011070b0cbb
Sujal <sujalnageshrishi05122003@gmail.com>
2024-05-08 23:17:34 +0530

    LCM with any range of numbers

--- Math/LCM.py
+++ Math/LCM.py
@@ -1,18 +1,14 @@
-def compute_lcm(x, y):
+def LCM(arr):
+    max_num = max(arr)
 
-   # choose the greater number
-   if x > y:
-       greater = x
-   else:
-       greater = y
+    lcm = max_num
+    while True:
+        
+        if all(lcm%i == 0 for i in arr):
+            break
+        lcm+=max_num
 
-   while(True):
-       if((greater % x == 0) and (greater % y == 0)):
-           lcm = greater
-           break
-       greater += 1
+    return lcm
+x = list(map(int, input("Enter integers separated by spaces: ").split()))
+print(LCM(x))
 
-   return lcm
-x = int(input("Enter 1st value : "))
-y = int(input("Enter 2ed value : "))
-print(compute_lcm(x,y))
